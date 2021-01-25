6ad98792741b3fa52e1d8f7171ac8ade5e5a29ec
Primosz <valadin96@gmail.com>
2021-01-25 19:56:11 +0100

    Added all files to git

---
+++ app/src/main/java/com/example/portfolio/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.portfolio
+
+import android.content.Intent
+import android.net.Uri
+import android.net.Uri.parse
+import android.os.Bundle
+import android.os.Handler
+import android.util.DisplayMetrics
+import android.view.MotionEvent
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.LinearLayout
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    var isSkipped = false;
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        setContentView(R.layout.activity_main)
+
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+        val params = videoView.layoutParams as LinearLayout.LayoutParams
+        params.width = metrics.widthPixels
+      //  params.height = metrics.heightPixels
+       // params.leftMargin = 0
+        videoView.layoutParams = params
+                //specify the location of media file
+        val uri: Uri = parse("android.resource://" + getPackageName() + "/" + R.raw.animation2)
+        //Setting MediaController and URI, then starting the videoView
+                videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+        Handler().postDelayed({
+            if (!isSkipped) {
+                val intent = Intent(this@MainActivity, MainScreen::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 6000)
+
+
+        videoView.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                isSkipped = true
+                Toast.makeText(this@MainActivity, "Skipped", Toast.LENGTH_LONG).show()
+                val intent = Intent(this@MainActivity, MainScreen::class.java)
+                startActivity(intent)
+                finish()
+                return true
+            }
+        })
+    }
+}
